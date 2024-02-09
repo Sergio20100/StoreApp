58f1f2323b82557c81f24b2be5a3834addb9e60e
sergio <smendoza@cotecmar.com>
2024-02-09 14:10:43 -0500

    mi primer commit, organizando la estructura de carpetas del proyecto

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  img = 'https://picsum.photos/250/250?r='+ Math.random()
+}
